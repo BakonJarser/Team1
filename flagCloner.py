26109804e81ca4aa9dbf28fa088a8b020ca9c030
jason <jason.ray.barker@jasonraybarker.com>
2020-02-08 20:56:13 -0800

    Initial commit of flag cloner

---
+++ flagCloner.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from os import listdir
+from os.path import isfile, join, dirname, abspath
+import sys
+import random
+
+NUM_COPIES = 2
+randomChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+# if sys.argv[1] is None:
+#     print("Usage: flagCloner <flag directory>")
+#     exit(1)
+
+path = dirname(abspath(__file__))
+# path = sys.argv[1]
+files = [file for file in listdir(path) if isfile(join(path, file))]
+
+for filename in files:
+    start = filename.find('_') + 1
+    end = filename.find('.')
+    if start < 0 or end < 0 or start > end:
+        continue
+    part_to_replace = filename[start:end]
+    for i in range(0, NUM_COPIES):
+        # generate a random replacement string
+        tempFilename = ""
+        for j in range(len(part_to_replace)):
+            tempFilename += random.choice(randomChars)
+        newFileName = filename.replace(part_to_replace, tempFilename)
+        # TODO write the new file
+        open(newFileName, "w+")
+    print(filename)
+
+
